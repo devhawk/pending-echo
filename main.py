6a7297c8d4974a3733f14cd673c68d272bc903c0
Harry Pierson <harry.pierson@dbos.dev>
2024-12-05 22:11:19 -0800

    basic .then working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import asyncio
+import functools
 from typing import Any, Callable, Coroutine, TypeVar, Generic
 
 T = TypeVar("T")
@@ -6,26 +7,40 @@ R = TypeVar("R")
 
 
 class Immediate(Generic[T]):
-    __slots__ = ("_func")
+    __slots__ = "_func"
 
     def __init__(self, func: Callable[[], T]):
         self._func = func
 
-    def then[R](self, func: Callable[[T], R]) -> "Immediate[R]":
-        raise NotImplementedError()
+    def then(self, next: Callable[[Callable[[], T]], R]) -> "Immediate[R]":
+        return Immediate(lambda: next(self._func))
 
     def __call__(self) -> T:
         return self._func()
 
 
 class Pending(Generic[T]):
-    __slots__ = ("_func")
+    __slots__ = "_func"
 
     def __init__(self, func: Callable[[], Coroutine[Any, Any, T]]):
         self._func = func
 
-    def then[R](self, func: Callable[[T], R]) -> "Pending[R]":
-        raise NotImplementedError()
+    @staticmethod
+    def _raise(ex: BaseException):
+        raise ex
+
+    @staticmethod
+    async def _do(
+        func: Callable[[], Coroutine[Any, Any, T]], next: Callable[[Callable[[], T]], R]
+    ) -> R:
+        try:
+            value = await func()
+            return next(lambda: value)
+        except BaseException as exp:
+            return next(lambda: Pending._raise(exp))
+
+    def then(self, next: Callable[[Callable[[], T]], R]) -> "Pending[R]":
+        return Pending[R](lambda: Pending._do(self._func, next))
 
     async def __call__(self) -> T:
         return await self._func()
@@ -36,5 +51,22 @@ def get_answer():
 
 
 async def get_question():
-    await asyncio.sleep(1)
+    await asyncio.sleep(0.1)
     return "What is 6 * 9?"
+
+
+def print_result(func: Callable[[], str]) -> str:
+    try:
+        result = func()
+        print(result)
+        return result
+    except BaseException as exp:
+        print(exp)
+        raise
+
+
+s = Immediate(get_answer).then(print_result)
+s()
+
+a = Pending(get_question).then(print_result)
+asyncio.run(a())
